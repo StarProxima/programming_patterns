bfea5cece1304ee57db4a698092479c1a7e9db32
StarProxima <maks.prozorov1@yandex.ru>
2023-03-18 10:38:19 +0300

    DataList draft

---
+++ lab2/task3/data_list.rb
@@ -0,0 +1,53 @@
+class DataList
+    private_class_method :new
+
+    private
+  
+    attr_reader :objects
+    attr_accessor :selected_num
+
+    public
+  
+    attr_writer :objects
+  
+    def initialize(objects)
+      self.objects = objects
+    end
+  
+    def select_element(number)
+      self.selected_num = number < objects.size ? number : nil
+    end
+  
+    def selected_id
+      objects[selected_num].id
+    end
+  
+    def data_table
+      result = []
+      counter = 0
+      objects.each do |obj|
+        row = []
+        row << counter
+        row.push(*table_fields(obj))
+        result << row
+        counter += 1
+      end
+      DataTable.new(result)
+    end
+  
+    def append(*objects)
+      self.objects.append(*objects)
+    end
+  
+    protected
+  
+    # Список значений полей для DataTable. Переопределить в наследниках
+    def table_fields(_obj)
+      []
+    end
+  
+    # Имена атрибутов объектов по порядку. Переопределить в наследниках
+    def column_names
+      []
+    end
+end
