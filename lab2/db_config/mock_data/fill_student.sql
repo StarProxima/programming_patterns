e1eef1b4f4a86bf0646fb7a69858d1b7e3ac2e0e
StarProxima <maks.prozorov1@yandex.ru>
2023-04-15 10:34:29 +0300

    Db config

---
+++ lab2/db_config/mock_data/fill_student.sql
@@ -0,0 +1,4 @@
+insert into student(last_name, first_name, father_name, phone, telegram, email, git) values
+    ('Хахук', 'Рустам', 'Нальбиевич', '79186733293', 'deltanull', 'deltanull@vk.ru', 'deltanull'),
+    ('Ашотов', 'Александр', 'Васеевич', NULL, 'ashottelega', 'ashot@mail.ru', 'ashotgit'),
+    ('Нагалевский', 'Артем', 'Великий', '79999999999', 'jakekepps', 'jakekepps@gmail.com', 'jakekepps');
