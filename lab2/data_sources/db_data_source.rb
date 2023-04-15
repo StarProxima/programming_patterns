4391a11b4e7bc10f964084e8681a6021e70a9601
StarProxima <maks.prozorov1@yandex.ru>
2023-04-15 10:45:34 +0300

    Data base data source

---
+++ lab2/data_sources/db_data_source.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'mysql2'
+
+class DBDataSource
+  private_class_method :new
+  @instance_mutex = Mutex.new
+
+  def initialize
+    # TODO: change lab2 name
+    db_config = YAML.load_file('./lab2/db_config/config.yaml').transform_keys(&:to_sym)
+    @client = Mysql2::Client.new(db_config)
+    @client.query_options.merge!(symbolize_keys: true)
+  end
+
+  def self.instance
+    return @instance if @instance
+
+    @instance_mutex.synchronize do
+      @instance ||= new
+    end
+
+    @instance
+  end
+
+  def prepare_exec(statement, *params)
+    @client.prepare(statement).execute(*params)
+  end
+
+  def query(statement)
+    @client.query(statement)
+  end
+end
